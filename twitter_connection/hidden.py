b595c6e1063686b49f12314d73416b97cca3173c
Pankaj Kumar <pkmahan786@gmail.com>
2017-01-05 18:46:07 +0530

    parameter alter

--- twitter_connection/hidden.py
+++ twitter_connection/hidden.py
@@ -1,8 +1,9 @@
+# Note : app the authentication keys or parameter are alterd. Please use yur own keys
 def oauth():
-    dict = {"customerKey": "rAg42HW0mKdrA6G6opld9yQGK",
-            "customerSecret": "qjXgYfL4yINseO6OXo7itiTplTynRYAWrS909RmTerdGtlHYvs",
-            "accessToken": "2356423920-i0BzG3etuyDR16RcmvfVZyz5v5ssbA3NDYhXTD8",
-            "accessTokenSecret": "cCTTSnR3rgEWODVg2eDvjMBD7IIj52YmnTei4Chpav6PZ"}
+    dict = {"customerKey": "rAg42HW0mKdd9yQGK",     
+            "customerSecret": "qjXgYeO6OXo7itiTplTynRYAWrS909RmTerdGtlHYvs",
+            "accessToken": "2356423920-i0BzG3e6RcmvfVZyz5v5ssbA3NDYhXTD8",
+            "accessTokenSecret": "cCTTSnR3ODVg2eDvjMBD7IIj52YmnTei4Chpav6PZ"}
     return dict
 
 # https://api.twitter.com/1.1/statuses/user_timeline.json?    api for tweet from specific user timeline
